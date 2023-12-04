618499e6ac9d781b346d1e91c31e0f0b85cefc10
cjacobme <cjacobme@aol.com>
2023-12-04 20:14:11 +0100

    MR comments

--- src/main/java/cj/software/genetics/schedule/api/validation/AllTasksDispatchedOnce.java
+++ src/main/java/cj/software/genetics/schedule/api/validation/AllTasksDispatchedOnce.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 
 @Documented
 @Constraint(validatedBy = AllTasksDispatchedOnceValidator.class)
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE})
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AllTasksDispatchedOnce {
 
