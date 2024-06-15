837cae3ad0f82b87ce4e486da51e3155de31e6c7
cjacobme <cjacobme@aol.com>
2024-06-15 22:19:08 +0200

    concrete example for square

--- src/main/java/cj/software/genetics/schedule/api/entity/FitnessProcedure.java
+++ src/main/java/cj/software/genetics/schedule/api/entity/FitnessProcedure.java
@@ -15,9 +15,41 @@ public enum FitnessProcedure {
     STD_DEVIATION,
 
     /**
-     * the fitness value is calculated as 1.0 divided by the squared sum. If we for example 3 priorities, we calculate
-     * the duration of each priority and square them. We then add all squared values. From that sum, we take the square
-     * root. The fitness value is then the 1.0 divided by this square root.
+     * the fitness value is calculated as 1.0 divided by the squared sum. If we for example 3 priorities with these
+     * durations:
+     *
+     * <table style="border: 1px solid black;">
+     *     <tr>
+     *         <th style="border: 1px solid black;">Priority</th>
+     *         <th style="border: 1px solid black;">duration</th>
+     *         <th style="border: 1px solid black;">scaling factor</th>
+     *         <th style="border: 1px solid black;">operation</th>
+     *         <th style="border: 1px solid black;">squared value</th>
+     *     </tr>
+     *     <tr>
+     *         <td style="border: 1px solid black;">1</td>
+     *         <td style="border: 1px solid black;">40 s</td>
+     *         <td style="border: 1px solid black;">3</td>
+     *         <td style="border: 1px solid black;">40 * 40 * 3</td>
+     *         <td style="border: 1px solid black;">4800</td>
+     *     </tr>
+     *     <tr>
+     *         <td style="border: 1px solid black;">2</td>
+     *         <td style="border: 1px solid black;">70 s</td>
+     *         <td style="border: 1px solid black;">2</td>
+     *         <td style="border: 1px solid black;">70 * 70 * 2</td>
+     *         <td style="border: 1px solid black;">9800</td>
+     *     </tr>
+     *     <tr>
+     *         <td style="border: 1px solid black;">3</td>
+     *         <td style="border: 1px solid black;">35 s</td>
+     *         <td style="border: 1px solid black;">1</td>
+     *         <td style="border: 1px solid black;">35 * 35</td>
+     *         <td style="border: 1px solid black;">1225</td>
+     *     </tr>
+     * </table>
+     * <p>
+     * The sum of all the values in the last column is 15825. The square root of that is 125.7975
      */
     SQUARED
 }
