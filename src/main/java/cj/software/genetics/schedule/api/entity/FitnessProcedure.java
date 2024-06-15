da0e05549405800c4e83dc4bf2865980ae38dcc9
cjacobme <cjacobme@aol.com>
2024-06-15 20:45:53 +0200

    added squared fitnesss procedure

--- src/main/java/cj/software/genetics/schedule/api/entity/FitnessProcedure.java
+++ src/main/java/cj/software/genetics/schedule/api/entity/FitnessProcedure.java
@@ -12,5 +12,12 @@ public enum FitnessProcedure {
     /**
      * the fitness value is calculated as 1.0 divided by the standard deviation of the durations of all workers
      */
-    STD_DEVIATION
+    STD_DEVIATION,
+
+    /**
+     * the fitness value is calculated as 1.0 divided by the squared sum. If we for example 3 priorities, we calculate
+     * the duration of each priority and square them. We then add all squared values. From that sum, we take the square
+     * root. The fitness value is then the 1.0 divided by this square root.
+     */
+    SQUARED
 }
